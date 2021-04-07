9ca5000cffdf1d08c12e06dd9002045e11cb3e5d
Jared <paubelj45@gmail.com>
2021-04-07 10:10:29 -0500

    Initial Commit

---
+++ iExpense-Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense-Project
+//
+//  Created by student on 4/7/21.
+//  Copyright © 2021 IN185 BS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
